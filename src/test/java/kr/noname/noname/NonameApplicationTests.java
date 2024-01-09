fe6fbb9fc65b8be2f664899034c7a0daf877da25
lutree <lutree@outlook.kr>
2024-01-09 13:20:50 +0900

    Initial commit

---
+++ src/test/java/kr/noname/noname/NonameApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.noname.noname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
